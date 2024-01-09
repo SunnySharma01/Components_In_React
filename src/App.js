be1867d0c42939638b7d62aa7eec35601df38dab
SunnySharma01 <sunny.kumar.che21@heritageit.edu.in>
2024-01-09 09:19:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import Subcomponents from './Components/Subcomponets'
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div className='border'>
+      <h1>Hello</h1>
+      <p><b>components</b></p>
+      <Subcomponents></Subcomponents>
     </div>
   );
 }
-
 export default App;
---
+++ src/Components/Subcomponets.js
@@ -0,0 +1,7 @@
+import './Subcomponents.css'
+function Subcomponents(){
+    return(
+        <h1 className="red">This is Subcomponents</h1>
+    );
+}
+export default Subcomponents;
